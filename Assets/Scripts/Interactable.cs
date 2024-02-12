55d68b430b499f6f0fb947f83899bfde81df6232
Joe Zlonicky <joe.zlonicky@gmail.com>
2024-02-12 11:22:57 -0800

    Interact

--- Assets/Scripts/Interactable.cs
+++ Assets/Scripts/Interactable.cs
@@ -1,18 +1,52 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Serialization;
 
+[Serializable]
+public class InteractEvent : UnityEvent {}
+
+[RequireComponent(typeof(Rigidbody2D))]
 public class Interactable : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject toggleActiveWhenWithinRange;
+    public InteractEvent onInteract;
+
+    private int _insideCount = 0;
+
+    private void Awake()
     {
-        
+        if (toggleActiveWhenWithinRange != null)
+        {
+            toggleActiveWhenWithinRange.SetActive(false);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        
+        _insideCount += 1;
+        if (_insideCount == 1 && toggleActiveWhenWithinRange != null)
+        {
+            toggleActiveWhenWithinRange.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        _insideCount -= 1;
+        Debug.Assert(_insideCount >= 0);
+
+        if (_insideCount == 0 && toggleActiveWhenWithinRange != null)
+        {
+            toggleActiveWhenWithinRange.SetActive(false);
+        }
+    }
+
+    public void CheckForInteraction()
+    {
+        if (_insideCount > 0)
+        {
+            onInteract?.Invoke();
+        }
     }
 }
